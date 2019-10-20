b4b0234024a48eb0e26a967560d623ffadd3f6e4
gangulyarin <gangulyarin@users.noreply.github.com>
2019-10-20 09:53:20 +0530

    Final Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'object-list';
+  supes_initial=[
+    {id:1, name:"Superman"}, {id:2, name:"Batman"}, {id:3, name:"Wonder Woman"}]
+
+supes_modified=[
+  {id:1, name:"Superman"}, {id:2, name:"Batman"}, {id:3, name:"Wonder Woman"},
+  {id:4, name:"Flash"}
+];
+supes=[];
+
+  constructor(){
+    this.supes=this.supes_initial;
+  }
+
+  refresh(){
+    this.supes=this.supes_modified;
+  }
+
+  recognize(index, item){
+    return item.id;
+  }
 }
